3d53e57cb7f515fe308d565d9c9bedaeed3a40fb
pnhung177 <pnhung177@gmail.com>
2022-12-17 07:41:14 +0700

    Add the app.server start/stop testcase

---
+++ test/tdd/app-server-test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const Devebot = require("devebot");
+const chores = Devebot.require("chores");
+const envcloak = require("envcloak").instance;
+
+const app = require("../../app");
+
+describe("app.server", function() {
+  before(function() {
+    envcloak.setup({
+      DEVEBOT_FORCING_SILENT: "devebot,webserver",
+      LOGOLITE_FULL_LOG_MODE: "false",
+      LOGOLITE_ALWAYS_ENABLED: "all",
+      LOGOLITE_ALWAYS_MUTED: "all"
+    });
+    chores.clearCache();
+  });
+  //
+  after(function() {
+    envcloak.reset();
+    chores.clearCache();
+  });
+  //
+  it("app.server should be started/stopped properly", function(done) {
+    app.server.start().then(function() {
+      return app.server.stop();
+    }).then(function() {
+      done();
+    });
+  });
+});
